a57d5e2106dd956161f06aa3d93af7aac317e399
Sirbu Alexandru <sirbualexvladut3@yahoo.com>
2022-05-23 07:03:27 +0300

    init

---
+++ GenerareMeniuNavbar.js
@@ -0,0 +1,23 @@
+
+document.write(` \ 
+ <style>
+ nav{
+    background-color: black;
+}
+.nav-item{
+    overflow: hidden    ;
+    font-size: 30px;
+    text-align: center;
+    padding: 16px;
+    color: purple;
+}
+ </style>
+    <nav> 
+    <a class="nav-item" href="Homepage.html"> Homepage   </a> 
+   
+     <a class="nav-item" href="Carti.html"> Cărți</a>  
+     
+     <a class="nav-item" href="Contact.html"> Contact</a> 
+    </nav>  
+    ` ) ;
+  
